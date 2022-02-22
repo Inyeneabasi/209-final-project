d1ad06ca636ced9b09535af9629f371a0536f36e
Inyeneabasi <inyeneabasi.akpata@pau.edu.ng>
2022-02-22 20:56:24 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  
+  getHome():{}{
+    return {title: 'Home Page'};
+  }
+
+  getAboutUs():{}{
+    return {title: 'About Us Page'};
+  }
 }
