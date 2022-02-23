b8398cf5e0ec119948667d5204354945c2b862ef
Inyeneabasi <inyeneabasi.akpata@pau.edu.ng>
2022-02-23 11:51:14 +0100

    update for citizen

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitizenRegistrationModule } from './citizen-registration/citizen-registration.module';
+import { IdentityModule } from './identity/identity.module';
+import { CitizenModule } from './citizen/citizen.module';
+import { CitizenRegistrationModule } from './citizen-registration/citizen-registration.module';
 
 @Module({
-  imports: [],
+  imports: [CitizenRegistrationModule, CitizenModule, IdentityModule],
   controllers: [AppController],
   providers: [AppService],
 })
